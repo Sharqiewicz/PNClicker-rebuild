43784f141f79ebaab6a675ca7cd1c1ef4a7fd54e
Kacper Szarkiewicz <SzarkiewiczMail@gmail.com>
2019-09-10 14:02:04 +0200

    Loading different bioms foes works

--- src/components/GameScreen/FoeField.js
+++ src/components/GameScreen/FoeField.js
@@ -13,17 +13,30 @@ class FoeField extends Component {
     redrawFoe = (biom ,foe) => {
         document.querySelectorAll('.stat')[0].innerHTML = `Damage: ${biom[foe].damage}`;
         document.querySelectorAll('.stat')[1].innerHTML = `Health: ${biom[foe].health}`;
+        document.getElementById('enemy_name').innerHTML = `Name: ${biom[foe].name}`;
     }
 
     foeDraw = () => {
-        let randomBiom = 1; // then make it random
+        let randomBiom = Math.floor(Math.random() * 4); // then make it random
+        let randomEnemy = 0;
 
         switch(randomBiom){
-
             //candy
+            case 0:
+                randomEnemy = Math.floor(Math.random() * fireFoes.length);
+                this.redrawFoe(fireFoes ,randomEnemy);
+                break;
             case 1:
-                let randomEnemy = Math.floor(Math.random() * candyFoes.length);
-                this.redrawFoe(candyFoes ,randomEnemy);
+                randomEnemy = Math.floor(Math.random() * waterFoes.length);
+                this.redrawFoe(waterFoes, randomEnemy);
+                break;
+            case 2:
+                randomEnemy = Math.floor(Math.random() * slimeFoes.length);
+                this.redrawFoe(slimeFoes, randomEnemy);
+                break;
+            case 3:
+                randomEnemy = Math.floor(Math.random() * candyFoes.length);
+                this.redrawFoe(candyFoes, randomEnemy);
                 break;
             default:
                 console.log('default');
@@ -35,7 +48,9 @@ class FoeField extends Component {
     render(){
         return(
             <div className="FoeField">
+                <button className="btn btn-warning" onClick={this.foeDraw}>New Foe</button>
                 <div id="enemy_container" className="col-12 d-flex flex-column bg-warning">
+                    <div id="enemy_name" className="bg-primary"></div>
                     <div id="enemy_card" className="order-sm-2 order-md-1">
                         <div id="enemy_image">
 
