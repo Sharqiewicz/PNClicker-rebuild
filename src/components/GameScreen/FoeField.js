258924b20903c471d005021156ef1e42968df6f4
Kacper Szarkiewicz <SzarkiewiczMail@gmail.com>
2019-09-10 13:46:33 +0200

    Working random foe from file system

--- src/components/GameScreen/FoeField.js
+++ src/components/GameScreen/FoeField.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
 import candyFoes from './foesList/candy'
 
-function FoeField() {
-    console.log(candyFoes);
+class FoeField extends Component {
 
-    return (
-        <div className="FoeField">
-            <div id="enemy_container" className="col-12 d-flex flex-column bg-warning">
-                <div id="enemy_card" className="order-sm-2 order-md-1">
-                    <div id="enemy_image">
+    componentDidMount = () => {
+        this.foeDraw();
+    }
 
+    redrawFoe = (biom ,foe) => {
+        document.querySelectorAll('.stat')[0].innerHTML = `Damage: ${biom[foe].damage}`;
+        document.querySelectorAll('.stat')[1].innerHTML = `Health: ${biom[foe].health}`;
+    }
+
+    foeDraw = () => {
+        let randomBiom = 1; // then make it random
+
+        switch(randomBiom){
+
+            //candy
+            case 1:
+                let randomEnemy = Math.floor(Math.random() * candyFoes.length);
+                this.redrawFoe(candyFoes ,randomEnemy);
+                break;
+            default:
+                console.log('default');
+                break;
+        }
+
+    }
+
+    render(){
+        return(
+            <div className="FoeField">
+                <div id="enemy_container" className="col-12 d-flex flex-column bg-warning">
+                    <div id="enemy_card" className="order-sm-2 order-md-1">
+                        <div id="enemy_image">
+
+                        </div>
+                    </div>
+                    <div id="enemy_stats" className="d-flex justify-content-center flex-wrap order-sm-1 order-md-2">
+                        <div className="stat"></div>
+                        <div className="stat"></div>
                     </div>
                 </div>
-                <div id="enemy_stats" className="d-flex justify-content-center flex-wrap order-sm-1 order-md-2">
-                    <div className="stat">{candyFoes[0].damage}</div>
-                    <div className="stat">{candyFoes[0].health}</div>
-                </div>
             </div>
-        </div>
-    );
+        )};
 }
 
 export default FoeField;
