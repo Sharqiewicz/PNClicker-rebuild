4a9a9e160bf2290b98657ed133b814569017e616
Kacper Szarkiewicz <SzarkiewiczMail@gmail.com>
2019-09-12 20:28:42 +0200

    Add simple reducer

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './css/index.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import charakter from './reducers/charakter'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(charakter);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
