9a8aff1cf4ef8a2ebe3e482af222863921b982bd
Kacper Szarkiewicz <SzarkiewiczMail@gmail.com>
2019-09-12 20:28:30 +0200

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import GameScreen from './components/GameScreen'
+import GameScreen from './components/GameScreen';
+import WelcomeScreen from './components/WelcomeScreen';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 
 function App() {
 
   return (
-    <div className="App">
-      <GameScreen/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={WelcomeScreen} />
+          <Route path="/game" component={GameScreen} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
