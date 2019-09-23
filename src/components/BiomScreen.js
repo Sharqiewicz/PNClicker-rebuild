b157468e2e9a955d180eb66d40142f7c25511510
Kacper Szarkiewicz <SzarkiewiczMail@gmail.com>
2019-09-23 15:55:18 +0200

    Add Biom Icons

--- src/components/BiomScreen.js
+++ src/components/BiomScreen.js
@@ -3,6 +3,10 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import fireicon from '../img/icons/fire.png';
+import watericon from '../img/icons/water.png';
+import slimeicon from '../img/icons/slime.png';
+import candyicon from '../img/icons/candy.png';
 
 class BiomScreen extends Component {
 
@@ -34,19 +38,19 @@ class BiomScreen extends Component {
                         <h1 className="col-12 text-white" id="your_biom">Choose your Biom</h1>
 
                         <div className="biom_card col-sm-12 col-xl-3" id="0" onMouseOver={this.changeDescription} onClick={this.playGame}>
-                            <div id="0"></div>
+                            <div id="0"><img src={fireicon} id="0"/></div>
                         </div>
 
                         <div className="biom_card col-sm-12 col-xl-3" id="1" onMouseOver={this.changeDescription} onClick={this.playGame}>
-                            <div id="1"></div>
+                            <div id="1"><img src={watericon} id="1"/></div>
                         </div>
 
                         <div className="biom_card col-sm-12 col-xl-3" id="2" onMouseOver={this.changeDescription} onClick={this.playGame}>
-                            <div id="2"></div>
+                            <div id="2"><img src={slimeicon} id="2"/></div>
                         </div>
 
                         <div className="biom_card col-sm-12 col-xl-3" id="3" onMouseOver={this.changeDescription} onClick={this.playGame}>
-                            <div id="3"></div>
+                            <div id="3"><img src={candyicon} id="3"/></div>
                         </div>
 
                         <div className="biom_desc col-10">Hover on element to see the description!</div>
