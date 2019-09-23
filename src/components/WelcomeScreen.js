dd104b240b3dbd1a9f3adf853067ae4084e23bd9
Kacper Szarkiewicz <SzarkiewiczMail@gmail.com>
2019-09-23 12:50:01 +0200

    Text-shadow function

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -4,9 +4,28 @@ import { connect } from 'react-redux';
 
 class WelcomeScreen extends Component {
 
-    state = {
-        name: 'Fin The Human'
+    componentDidMount(){
+        this.changeBackground();
     }
+
+    constructor(){
+        super()
+        this.state = {
+            name: 'Fin The Human'
+        }
+    }
+
+    changeBackground = () => {
+        const colors_dark = ['#c23616', '#0097e6', '#44bd32', '#D980FA'];
+
+        setInterval(() => {
+            let randomBck = Math.floor(Math.random() * 4);
+            document.getElementById('WelcomeScreen').style.textShadow = `0.2em 0.2em 0.3em ${colors_dark[randomBck]}`;
+        }, 3000)
+
+    }
+
+
 
     newName = (e) => {
         this.setState({
